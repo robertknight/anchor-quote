f5412c8ea1d79ddc354578a8af9b4e55105d9f9f
Robert Knight <robertknight@gmail.com>
2019-05-27 23:10:54 +0100

    Upgrade approx-string-match
    This improves performance and provides its own type definitions.

--- types/approx-string-match/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "approx-string-match" {
-  interface Match {
-    start: number;
-    end: number;
-    errors: number;
-  }
-  export default function search(text: string, pattern: string, maxErrors: number): Match[];
-}
-
